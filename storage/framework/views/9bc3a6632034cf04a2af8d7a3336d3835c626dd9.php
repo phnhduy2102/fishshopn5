a45c2c079690e79e02acaaa78cccc44457768d34
phnhduy2102 <ppduy21@gmail.com>
2024-03-17 10:20:55 +0700

    sua 1 chut

--- storage/framework/views/9bc3a6632034cf04a2af8d7a3336d3835c626dd9.php
+++ storage/framework/views/9bc3a6632034cf04a2af8d7a3336d3835c626dd9.php
@@ -3,7 +3,7 @@
 <!-- Title Tag  -->
 <title><?php echo $__env->yieldContent('title'); ?></title>
 <!-- Favicon -->
-<link rel="icon" type="image/png" href="/storage/photos/1/banner/Final.png">
+<link rel="icon" type="image/png" href="/storage/photos/1/banner/final.png">
 <!-- Web Font -->
 <link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
 
